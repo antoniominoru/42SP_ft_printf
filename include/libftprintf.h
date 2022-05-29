3a85ec102fc0e409a2dff5d0ccf2d91e3cb72a1c
aminoru- <aminoru-@student.42sp.org.br>
2022-05-29 15:11:58 +0200

    ft_convert_s and ft_strndup

--- include/libftprintf.h
+++ include/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 21:10:53 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/05/28 15:08:45 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/05/29 15:10:46 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <wchar.h>
 # include "../libft/libft.h"
 
 # define P_HOLDER_ALL "-0# +123456789*"
@@ -24,6 +25,13 @@
 # define P_HOLDER_LEFT_PADS '0'
 # define P_HOLDER_STAR '*'
 # define P_HOLDER_PRECISION '.'
+
+# define DECIMAL_BASE "0123456789"
+# define HEXADECIMAL_L_BASE "0123456789abcdef"
+# define HEXADECIMAL_U_BASE "0123456789ABCDEF"
+# define OCTAL_BASE "01234567"
+
+# define SPACE ' '
 
 typedef struct s_format
 {
@@ -39,7 +47,7 @@ typedef struct s_holder
 	int		width;
 	int		precision;
 	char	padding;
-	char	conversion;
+	char	specifier;
 	char	*argument;
 	char	*prefix;
 	size_t	len;
@@ -55,14 +63,18 @@ t_holder	*ft_inicialize_placeholder(void);
 // Placeholder
 void	ft_placeholder(t_format *fmt);
 void	*ft_format_specifiers(t_format *fmt, t_holder *h);
+void	*ft_sub_specifier(t_format *fmt, t_holder *h);
 void	ft_fs_flags(t_format *fmt, t_holder *h);
 void	ft_fs_width(t_format *fmt, t_holder *h);
 void	ft_fs_precision(t_format *fmt, t_holder *h);
-void	ft_fs_conversion(t_format *fmt, t_holder *h);
+void	ft_fs_specifier(t_format *fmt, t_holder *h);
 
-// adicional function to LIBFT
+// Adicional function to LIBFT
 char	*ft_appendchr(char const *s, char const ch);
+char	*ft_strndup(const char *s1, size_t n);
 
-//
+// Convert functions
+void	ft_convert_c(t_format *fmt, t_holder *h);
+void	ft_convert_s(t_format *fmt, t_holder *h);
 
 #endif
